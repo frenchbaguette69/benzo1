900a996feb0b7e814227edb2324bf4390585bb30
jim <jimdedrummer99@gmail.com>
2024-08-25 19:33:36 +0200

    x

--- server/routers/order/orderproduct.router.ts
+++ server/routers/order/orderproduct.router.ts
@@ -2,6 +2,11 @@ import { router, publicProcedure } from '../../trpc';
 import z from 'zod'
 
 export const orderProductRouter = router({
-    insert: publicProcedure.input(z.object({ email: z.string() })).mutation(async ({ ctx: { client }, input }) => {
+    insert: publicProcedure.input(z.object({ quantity: z.number(), productId: z.number(), orderId: z.number() })).mutation(async ({ ctx: { client }, input }) => {
+
+        await client.orderProduct.create({
+            data: {quantity: input.quantity, product: { connect: { id: input.productId } }, order: { connect: {id: input.orderId}}}
+        })
+
     })
 })
