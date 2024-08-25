689f25ad7cfb97383a252df6f911fc1f4c5d3a2e
jim <jimdedrummer99@gmail.com>
2024-08-25 22:04:25 +0200

    latest changes from ji

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { getSession, useSession } from 'next-auth/react'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export async function middleware(request: NextRequest) {
+
+    const requestForNextAuth = {
+        headers: {
+          cookie: request.headers.get('cookie') ?? "",
+        },
+    };
+
+    const session = await getSession({req: requestForNextAuth});
+
+    if(!session?.user.isAdmin){
+        return NextResponse.redirect(new URL('/admin/login', request.url))
+    }
+
+    return NextResponse.redirect(new URL('/', request.url))
+}
+ 
+export const config = {
+  matcher: '/admin/dashboard/:path*',	
+}
--- server/routers/user/user.router.ts
+++ server/routers/user/user.router.ts
@@ -2,8 +2,20 @@ import { router, publicProcedure } from '../../trpc';
 import z from 'zod'
 
 export const userRouter = router({
-    register: publicProcedure.input(z.object({ email: z.string(), name: z.string(), shopId: z.number().nullish() })).mutation(async () => {
+    login: publicProcedure.input(z.object({ email: z.string(), password: z.string() })).mutation(async ({ctx: { client }, input}) => {
 
-        return 'Hello World';
+        const user = await client.user.findUnique({
+            where: { email: input.email }
+        })
+
+        if (!user) {
+            throw new Error("User not found")
+        }
+
+        if (user.password !== input.password) {
+            throw new Error("Invalid password")
+        }
+
+        return user;
     })
 })
