50745d99d547ccd05b96e88e46ab626804719306
frenchbaguette69 <jonkovoorjou@gmail.com>
2024-08-25 23:23:30 +0200

    product page
    product page

---
+++ pages/products/test.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/header";
+import Footer from "@/components/footer";
+
+const Productpage = () => {
+  return (
+    <div>
+        <div className="container">
+            
+        </div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Productpage;
